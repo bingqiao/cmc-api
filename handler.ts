4020ad60595fd807679846a7aec06c26ad46ddb0
Bing Qiao <bqiaodev@gmail.com>
2019-08-13 22:39:21 +0100

    getting listing from coinmarketcap api

--- handler.ts
+++ handler.ts
@@ -1,12 +1,31 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
+const https = require('https');
+
+export const getListing = async (_event, _context) => {
+  const options = {
+    hostname: 'sandbox-api.coinmarketcap.com',
+    headers: {
+      'X-CMC_PRO_API_KEY': 'PLACE_HOLDER'
+    },
+    path: '/v1/cryptocurrency/listings/latest?start=1&limit=5000&convert=USD'
   };
+  return new Promise((resolve, reject) => {
+    https.get(options, function(res) {
+        console.log("Got response: " + res.statusCode);
+        var body = '';
+        res.on('data', function(chunk) {
+          body += chunk;
+        });
+        res.on('end', function() {
+          resolve({
+            statusCode: 200,
+            body: body
+          });
+        });
+    }).on('error', function(e) {
+        console.log("Got error: " + e.message);
+        reject(e);
+    });
+});
 }
