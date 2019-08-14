bdb0e2a8995f67c81a8bd24ec677da68a2da0a38
Bing Qiao <bqiaodev@gmail.com>
2019-08-14 15:40:35 +0100

    added quotes api

---
+++ README.md
@@ -0,0 +1,13 @@
+# CmcApi
+
+Backend service using CoinMarketCap API
+
+## API key
+
+An API key for CoinMarketCap sandbox environment is needed.
+
+Find and replace the value of `X-CMC_PRO_API_KEY` in handler.ts with the API key from CoinMarketCap API.
+
+## Frontend app
+
+Please visit github project [cmc-dashboard](https://www.github.com/bingqiao/cmc-dashboard) which serves data for this Angular frontend.
--- handler.ts
+++ handler.ts
@@ -2,33 +2,63 @@ import 'source-map-support/register';
 
 const https = require('https');
 
+const defaultOptions = {
+  hostname: 'sandbox-api.coinmarketcap.com',
+  headers: {
+    'X-CMC_PRO_API_KEY': 'PLACEHOLDER'
+  }
+};
+
+
 export const getListing = async (_event, _context) => {
-  const options = {
-    hostname: 'sandbox-api.coinmarketcap.com',
-    headers: {
-      'X-CMC_PRO_API_KEY': '4f269312-2491-4cd7-80fc-09aefdac3268'
-    },
-    path: '/v1/cryptocurrency/listings/latest?start=1&limit=5000&convert=USD'
-  };
+  const options = Object.assign(defaultOptions, {path: '/v1/cryptocurrency/listings/latest?start=1&limit=5000&convert=USD'});
   return new Promise((resolve, reject) => {
-    https.get(options, function(res) {
-        console.log("Got response: " + res.statusCode);
-        var body = '';
-        res.on('data', function(chunk) {
-          body += chunk;
+    https.get(options, function (res) {
+      console.log("Got response: " + res.statusCode);
+      var body = '';
+      res.on('data', function (chunk) {
+        body += chunk;
+      });
+      res.on('end', function () {
+        resolve({
+          headers: {
+            'Cache-Control': 'max-age=600',
+            'Access-Control-Allow-Origin': '*' // this is needed. it seems claudia.js otherwise enables this automatically. needs verification.
+          },
+          statusCode: 200,
+          body: body
         });
-        res.on('end', function() {
-          resolve({
-            headers: {
-              'Access-Control-Allow-Origin': '*' // this is needed. it seems claudia.js otherwise enables this automatically. needs verification.
-            },
-            statusCode: 200,
-            body: body
-          });
-        });
-    }).on('error', function(e) {
-        console.log("Got error: " + e.message);
-        reject(e);
+      });
+    }).on('error', function (e) {
+      console.log("Got error: " + e.message);
+      reject(e);
     });
-});
+  });
+}
+
+export const getQuotes = async (_event, _context) => {
+  let symbol = _event.pathParameters.symbol;
+  const options = Object.assign(defaultOptions, {path: `/v1/cryptocurrency/quotes/latest?symbol=${symbol}&convert=USD`});
+  return new Promise((resolve, reject) => {
+    https.get(options, function (res) {
+      console.log("Got response: " + res.statusCode);
+      var body = '';
+      res.on('data', function (chunk) {
+        body += chunk;
+      });
+      res.on('end', function () {
+        resolve({
+          headers: {
+            'Cache-Control': 'max-age=600',
+            'Access-Control-Allow-Origin': '*' // this is needed. it seems claudia.js otherwise enables this automatically. needs verification.
+          },
+          statusCode: 200,
+          body: body
+        });
+      });
+    }).on('error', function (e) {
+      console.log("Got error: " + e.message);
+      reject(e);
+    });
+  });
 }
